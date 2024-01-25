363cbdeacf74f1c719b2c0dd1860a4d3105ef569
offorgen <157226311+offorgen@users.noreply.github.com>
2024-01-25 11:45:41 +0200

    Sravnenie

---
+++ sravnenie.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$x = 1.0;
+$y = 1;
+
+var_dump($x === $y);
+
+$a = '';
+$b = false;
+var_dump($a == $b);
+
+$a = (int)1;
+$b = true ;
+
+var_dump($a != $b );
+
+$a = (int)1;
+$b = true ;
+var_dump($a !== $b);
+
+$a = false;
+$b = (bool)0 ;
+
+var_dump($a <=> $b);
+
+$a = (bool)null;
+$b = (bool)'';
+
+var_dump($a == $b);

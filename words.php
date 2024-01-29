5f92308dd831c4550a6455d63b3adb7eed55a3af
offorgen <157226311+offorgen@users.noreply.github.com>
2024-01-29 20:24:21 +0200

    Add files via upload

---
+++ words.php
@@ -0,0 +1,21 @@
+<?php
+
+$string = 'Hello world. I\'m a human!';
+
+
+//var_dump(strlen($string));
+
+//
+//    if (($result = strpos($string, '.')) !== false){
+//        $position = substr($string , $result + 1);
+//        echo $position . PHP_EOL;
+//
+//}
+
+
+var_dump(strlen($string));
+
+
+if (($result = strpos($string, '.')) !== false){
+    $position = substr($string , $result + 1);
+    echo ltrim($position . PHP_EOL); }

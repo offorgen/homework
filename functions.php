d8b9fbceb389aa655ec1e043fcc1e6e9ca26cead
offorgen <157226311+offorgen@users.noreply.github.com>
2024-01-31 23:42:50 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,35 +1,31 @@
 <?php
 
-
-function S( int|float $number1): int|float
+function S(int|float $number1): int|float
 {
-    return pi() * ($number1 **2);
+    return pi() * ($number1 ** 2);
 
 }
+
 $number = 5;
 $s = S($number);
-echo $s . PHP_EOL ;
+echo $s . PHP_EOL;
 
 
-
-
-
-function degree(int|float $number1 , int|float $pow)
+function degree(int|float $number1, int|float $pow)
 {
     return $number1 ** $pow;
 
 }
 
-$result = degree( 15 , 2);
+$result = degree(15, 2);
 echo $result . PHP_EOL;
 
 
-
-
-function degree2(int|float &$number, int|float$pow)
+function degree2(int|float &$number, int|float $pow)
 {
     $number **= $pow;
 }
+
 $number3 = 5;
-degree2($number3 , 2);
+degree2($number3, 2);
 echo $number3 . PHP_EOL;
